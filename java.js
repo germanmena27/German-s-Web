09ed05cfb695b0f1efb282b2c35d70dda4616276
germanmena27 <163568223+germanmena27@users.noreply.github.com>
2024-03-15 17:50:29 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+const button= document.querySelector("button");
+button.onclick= function(){
+
+     let name=prompt ("¿Qué prenda de Nike elegirías?");
+	 alert ("Los/las " +name+ " son una gran elección.");
+};
